9507bbe073d820a3ec54511a57ccd514ba890470
Arunabh Das <arunabhdas@gmail.com>
2019-11-25 21:54:54 -0500

    List on detail screen

--- lib/detailscreen.dart
+++ lib/detailscreen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_coding_challenge/model/restaurant_restaurant.dart';
 import 'model/cuisine_cuisine.dart';
 import 'model/cuisine_element.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http/http.dart' show get;
+import 'model/restaurant_restaurant.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  @override
+  State createState() => new DyanmicList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,27 +22,16 @@ class DetailScreen extends StatelessWidget {
   }
 
 
-  Future<List<CuisineCuisine>> _loadRestaurantsList() async {
-    List<CuisineElement> cuisineElementsList = [];
-    List<CuisineCuisine> cuisineList = [];
-    var url = 'https://developers.zomato.com/api/v2.1/search?sort=cost&order=asc&category=1&cuisines=1035';
-    Map<String, String> headers = {"user-key": "327e75c31ca03dbb55cbabe4257acfa9", "Accept": "application/json"};
-    Response response = await get(url, headers: headers);
-    var data = json.decode(response.body);
+}
 
-    var rest = data['cuisines'] as List;
-
-    cuisineElementsList = rest.map<CuisineElement>( (json) => CuisineElement.fromJson(json)).toList();
-
-
-
-    for (CuisineElement cui in cuisineElementsList) {
-      CuisineCuisine cuisine = CuisineCuisine(cuisineId: cui.cuisine.cuisineId, cuisineName: cui.cuisine.cuisineName);
-      cuisineList.add(cuisine);
-    }
-
-
-    return cuisineList;
+class DyanmicList extends State<DetailScreen> {
+  List<String> litems = [];
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('DetailScreen'),
+      ),
+    );
   }
-
 }
